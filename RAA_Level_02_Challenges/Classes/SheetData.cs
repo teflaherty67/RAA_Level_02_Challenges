e8e60efc69dfc60b28307838d894957da0401f0e
Tom E Flaherty <teflaherty@utexas.edu>
2023-04-24 17:48:33 -0500

    in progress

---
+++ RAA_Level_02_Challenges/Classes/SheetData.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAA_Level_02_Challenges
+{
+    internal class SheetData
+    {
+        public string SheetNumber {  get; set; }
+        public string SheetName { get; set;}
+        public bool IsPlaceholder { get; set; }
+        public Element Titleblock { get; set; }
+    }
+}
